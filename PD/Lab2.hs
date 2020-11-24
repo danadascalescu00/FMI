93ddf940b63e22cafe75acf51fed6b77066a0b3c
Dăscălescu Dana <48893255+danadascalescu00@users.noreply.github.com>
2020-11-24 19:26:19 +0200

    Add files via upload

--- PD/Lab2.hs
+++ PD/Lab2.hs
@@ -1,128 +1,121 @@
 import Data.Char (isDigit, digitToInt)
 
--------------------------------------------------------------------------------
-------------------------------RECURSIE: FIBONACCI------------------------------
--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+------------------------------------RECURSIE------------------------------------
+--------------------------------------------------------------------------------
 fibonacciCazuri :: Integer -> Integer
-fibonacciCazuri n 
+fibonacciCazuri n
+    | n < 0 = error "n must be positive"
     | n < 2 = n
-    | otherwise = fibonacciCazuri(n-2) + fibonacciCazuri(n-1)
+    | otherwise = fibonacciCazuri (n - 2) + fibonacciCazuri ( n - 1)
 
 
+-- Alternativ, putem folosi o definitie in stil ecuational (cu sabloane):
+fibonacciEcuational :: Integer -> Integer
 fibonacciEcuational 0 = 0
 fibonacciEcuational 1 = 1
-fibonacciEcuational n = fibonacciEcuational(n-2) + fibonacciEcuational(n-1)
+fibonacciEcuational n = 
+    fibonacciEcuational (n - 1) + fibonacciEcuational (n - 2)
 
 
-fibonacciLiniar 0 = 1
+fibonacciLiniar :: Integer -> Integer
+fibonacciLiniar 0 = 0
 fibonacciLiniar n = snd (fibonacciPereche n)
-    where
+    where 
         fibonacciPereche 1 = (0,1)
-        fibonacciPereche n = (b,a+b)
-            where (a,b) = fibonacciPereche(n-1)
-            
--- let (a,b) = fibonacciPereche(n-1) in (b,b+a)
+        fibonacciPereche n = (b, a + b)
+            where (a, b) = fibonacciPereche (n - 1)
 
 
--- Exemplu 2 - Data fiind o lista de intregi, sa se scrie o functie semiPare care elimina din lista numere impare
--- si le injumatateste pe cele pare
-semiPare :: [Int] -> [Int]
-semiPare l
+-- Data fiind o lista de numere intregi, sa se scrie o functie semiPare care elimina numerele impare si le injumatateste pe cele pare
+semiPareRecDestr :: [Int] -> [Int]
+semiPareRecDestr l 
     | null l = l
-    | even h = h `div` 2 : t'
-    | otherwise = t'
-    where
+    | even h = h `div` 2 : semiPareRecDestr t
+    | otherwise = semiPareRecDestr t
+    where 
         h = head l
         t = tail l
-        t' = semiPare t
 
 
 semiPareRecEq :: [Int] -> [Int]
 semiPareRecEq [] = []
-semiPareRecEq (h:t)
-    | even h = h `div` 2 : t'
-    | otherwise = t'
-    where t' = semiPareRecEq t
+semiPareRecEq (h:t) = if even h then h `div` 2 : t' else t'
+    where 
+        t' = semiPareRecEq t
 
 
 semiPareComp :: [Int] -> [Int]
-semiPareComp l = [x `div` 2| x <- l, even x]
+semiPareComp l = [x `div` 2 | x <- l, even x]
 
 
----------------------------------------------------------------------------------------------------------------------------
-----------------------------------------------------------Exercitii--------------------------------------------------------
----------------------------------------------------------------------------------------------------------------------------
---1. Scrieti o functie in care date fiind limita inferioara si limita superioara(intregi) a unui interval inchis
--- si o lista de numere intrgi, returneaza lista numerelor din lista care apartin intervalului
-
--- Varianta 1 - folosind recursie
-inIntervalRec :: Int -> Int -> [Int] -> [Int]
-inIntervalRec _ _ [] = []
-inIntervalRec a b (h:t)
-    | h >= a && h <= b = h : (inIntervalRec a b t)
-    | otherwise = (inIntervalRec a b t)
-
--- Varianta 2 - folosind descrieri de liste
-inIntervalComp :: Int -> Int -> [Int] -> [Int]
-inIntervalComp a b l = [x | x <- l, a <= x && x <= b]
+-- [In interval] Scrieti o functie care date find limita inferioar si cea superioara (intregi) a unui 
+-- interval inchis si o list de numere intregi, calculeaza lista numerelor din lista care apartin intervalului.
+inInterval :: Ord a => a -> a -> [a] -> [a]
+inInterval inf sup l = [x | x <- l, x >= inf && x <= sup]
 
 
---2. Scrieti o functie care numara cate numere strict pozitive sunt intr-o lista data ca argument. 
-pozitiveRec :: [Integer] -> Integer
-pozitiveRec [] = 0
-pozitiveRec (x:xs)
-    | x > 0 = 1 + pozitiveRec(xs)
-    | otherwise = pozitiveRec(xs)
-
-pozitiveComp :: [Int] -> Int
-pozitiveComp l = length [ 1 | x<-l, x>0 ]
+-- [Numaram pozitive] Scrieti o functie care numara cate numere strict pozitive sunt intr-o lista data ca argument
+numPozitive :: (Ord a, Num a) => [a] -> Int
+numPozitive l = length [x | x <- l , x > 0]
 
 
---3. Scrieti o functie care returneaza lista pozitiilor numerelor impare din lista data ca parametru
+-- [Pozitii] Scrieti o functie care dat find o lista de numere calculeaza lista pozitiilor elementelor impare din lista originala
+pozitiiImpareAux :: (Integral a1, Num a2) => [a1] -> a2 -> [a2]
 pozitiiImpareAux [] _ = []
-pozitiiImpareAux (x:xs) a 
-    | odd x = a : (pozitiiImpareAux xs (a+1))
-    | otherwise = pozitiiImpareAux xs (a+1)
+pozitiiImpareAux (x:xs) a
+    | odd x = a : pozitiiImpareAux xs (a + 1)
+    | otherwise = pozitiiImpareAux xs (a + 1)
 
-pozitiiImpareRec :: [Int] -> [Int]
-pozitiiImpareRec [] = []
-pozitiiImpareRec l = pozitiiImpareAux l 0
-
-
-pozitiiImpareComp l = [index | (x,index) <- zip l [0..] , odd x]
+pozitiiImpare :: (Integral a1, Num a2) => [a1] -> [a2]
+pozitiiImpare l = pozitiiImpareAux l 0
 
 
---4. [MultDigit] Scrietti o functie care calculeaza produsul tuturor cifrelor care apar in sirul de caractere dat ca intrare. 
--- Daca nu sunt cifre in sir, raspunsul functiei trebuie sa fie 1.
+pozitiiImpareComp :: (Integral a1, Num a2, Enum a2) => [a1] -> [a2]
+pozitiiImpareComp l = [index | (x, index) <- zip l [0..], odd x]
+
+
+-- [MultDigit] Scrieti o functie care calculeaza produsul tuturor cifrelor care apar in sirul de caractere dat ca intrare. 
+-- Daca nu sunt cifre in sir raspunsul functiei trebuie sa fie 1.
 multDigitsRec "" = 1
 multDigitsRec (c:sir)
-    | isDigit c = (digitToInt c) * multDigitsRec sir
+    | isDigit c = digitToInt c * multDigitsRec sir
     | otherwise = multDigitsRec sir
+
 
 multDigitsComp :: String -> Int
 multDigitsComp sir = product [ digitToInt ch | ch <- sir, isDigit ch ]
 
 
---5 [Discount] Scrieti o functie care pentru o list de valori (reprezentand niste preturi) aplica un discount 
+-- [Discount] Scrieti o functie care pentru o list de valori (reprezentand niste preturi) aplica un discount 
 -- de 25% acelor valori si pastreaz in list doar valorile reduse care sunt mai mici decat 200.
+discountRec :: (Ord a, Fractional a) => [a] -> [a]
 discountRec [] = []
 discountRec (x:xs)
     | 0.75 * x < 200 = 0.75 * x : discountRec xs
     | otherwise = discountRec xs
 
+discountRec2 :: (Ord a, Fractional a) => [a] -> [a]
+discountRec2 [] = []
+discountRec2 (x:xs) = if discount < 200 then discount : xs' else xs'
+    where
+        discount = 0.75 * x
+        xs' = discountRec xs
 
-discountComp l = [0.75 * x | x <- l, 0.75 * x < 200]
 
-
-discount :: Float -> Float     
+discount :: Fractional a => a -> a
 discount x = x - 0.25 * x
 
-discountRec2 :: [Float] -> [Float]
-discountRec2 [] = []
-discountRec2 (x:xs) 
-   | discount x < 200 =  discount x : discountRec xs
-   | otherwise = discountRec xs
+discountRec3 :: (Ord a, Fractional a) => [a] -> [a]
+discountRec3 [] = []
+discountRec3 (x:xs)
+    | discount x < 200 = discount x : discountRec xs
+    | otherwise = discountRec xs
+
+
+discountComp :: (Ord a, Fractional a) => [a] -> [a]
+discountComp l = [ 0.75 * val | val <- l, 0.75 * val < 200 ]
 
 
 discountComp2 :: [Float] -> [Float]
-discountComp2 xs = [ y | x<- xs, let y = discount x, y < 200]
+discountComp2 xs = [ y | x<- xs, let y = discount x, y < 200 ]
--- PD/Lab3.hs
+++ PD/Lab3.hs
@@ -1,104 +1,131 @@
-import  Data.List
+import Data.List ()
+
+-- [Denirea listelor prin comprehensiune]
+list1 = [ x ^ 2 | x <- [1..10], x `rem` 3 == 2 ] 
+list2 = [ (x,y) | x <- [1..5], y <- [x..(x+2)]] 
+list3 = [ (x,y) | x <- [1..3], let k = x ^ 2,  y <- [1..k]] 
+list4 = [ x | x <- " Facultatea de Matematica si Informatica " , x `elem`  ['A'..'Z'] ] 
+list5 = [ [x,y] | x <- [1..5], y <- [1..5], x < y ]
+
+
 
 factori :: Int -> [Int]
-factori x = [ n | n <- [1..x], x `mod` n == 0 ]
+factori n = [ x | x <- [1..n] , n `mod` x == 0]
 
 prim :: Int -> Bool
-prim x = factori x == [1,x]
+prim n = length ( factori n ) == 2
 
 numerePrime :: Int -> [Int]
-numerePrime n = [num | num <- [2..n], prim num]
+numerePrime n = [ x | x <- [2..n], prim x]
 
-myzip3 :: [Int] -> [Int] -> [Int] -> [(Int, Int, Int)]
+
+myzip3 :: [Integer] -> [Integer] -> [Integer] -> [(Integer, Integer, Integer)]
 myzip3 _ _ [] = []
 myzip3 _ [] _ = []
 myzip3 [] _ _ = []
 myzip3 (x:xs) (y:ys) (z:zs) = (x,y,z) : myzip3 xs ys zs
 
-myZip3 l1 l2 l3 = zipAux (zip l1 l2) l3
+
+myZip3 :: [a] -> [b] -> [c] -> [(a, b, c)]
+myZip3 l1 l2 = myzipAux ( zip l1 l2 )
     where 
-        zipAux _ [] = []
-        zipAux [] _ = []
-        zipAux ((x,y):xys) (z:zs) = (x,y,z) : zipAux xys zs
+        myzipAux _ [] = []
+        myzipAux [] _ = []
+        myzipAux ((x,y):xys) (z:zs) = (x,y,z) : myzipAux xys zs
 
 
---------------------------------------------------------
-----------FUNCTII DE NIVEL INALT -----------------------
---------------------------------------------------------
+----------------------------------------------------------------------
+----------------------- FUNCTII DE NIVEL INALT -----------------------
+----------------------------------------------------------------------
+
 aplica2 :: (a -> a) -> a -> a
+-- aplica2 f x =  f $ f x
 aplica2 f x = f (f x)
---aplica2 f = f.f
+-- aplica2 f = f . f
 --aplica2 f = \x -> f (f x)
 --aplica2  = \f x -> f (f x)
 
--- firstElem l = map(\ (x, y) -> x) l
-firstElem :: [(b1, b2)] -> [b1]
-firstElem = map fst 
 
-sumList :: [[Int]] -> [Int]
+firstEl :: [(a,b)] -> [a]
+firstEl = map fst
+
+secondEl :: [(a,b)] -> [b]
+secondEl = map snd
+
+thirdEl :: [(a,b,c)] -> [c]
+thirdEl = map(\(_,_,c) -> c)
+
+
+sumList :: [[Integer]] -> [Integer]
 sumList = map sum
 
+
 prel2 :: [Integer] -> [Integer]
-prel2 = map (\x -> if even x then x `div` 2 else x*2)
+prel2 =  map prelucreaza
+    where
+        prelucreaza x
+            | even x = x `div` 2
+            | otherwise = x * 2
 
-filtrareSir :: (Foldable t, Eq a) => a -> [t a] -> [t a]
-filtrareSir ch = filter (elem ch)
 
-patrateImpare :: [Integer] -> [Integer]
-patrateImpare = map (^2) . filter odd
+prel2' :: [Integer] -> [Integer]
+prel2' = map (\x -> if even x then x `div` 2 else x * 2)
 
-patratePozitiiImpare :: [Integer] -> [Integer]
-patratePozitiiImpare = map ( (^2) . snd) . filter (odd . fst) . zip [0..]
+prel2Comp :: Integral a => [a] -> [a]
+prel2Comp l = [k | x <- l, let k = if even x then x `div` 2 else x * 2]
 
-eliminaConsoane :: [[Char]] -> [[Char]]
-eliminaConsoane = map (filter (`elem` "aeiouAEIOU"))
 
-mymap :: (t -> a) -> [t] -> [a]
-mymap _ [] = []
-mymap f (x:xs) = f x : mymap f xs
+filtrareSir :: Char -> [String] -> [String]
+filtrareSir ch =  filter (elem ch)
+
+
+numereImpare :: [Integer] -> [Integer]
+numereImpare = map (^2) . filter odd 
+
+
+pozitiiImpare :: [Integer] -> [Integer]
+pozitiiImpare = map ((^2) . snd ) . filter(odd . fst) . zip [0..] 
+
+
+numaiVocale :: [[Char]] -> [[Char]]
+numaiVocale = map $ filter( `elem` "aeiouAEIOU") 
+
+
+mymap :: (a -> b) -> [a] -> [b]
+mymap f l = [ f x | x <- l]
 
 myfilter :: (a -> Bool) -> [a] -> [a]
 myfilter _ [] = []
-myfilter p (x:xs)
-    | p x = x : myfilter p xs
-    | otherwise = myfilter p xs
+myfilter f (x:xs) = if f x then x : myfilter f xs else myfilter f xs
 
 
-sieveEratosthenes n = sieveRec [2..n]
-    where
-        sieveRec [] = []
-        sieveRec (x:xs) = x : sieveRec (filter(\n -> n `rem` x /= 0) xs)
-
-numerePrimeCiur n = sieve [2..n]
-          where
-          sieve [] = []
-          sieve (p:xs) = p : sieve [x | x <- xs, rem x p > 0]
+eratostheneSieve :: Int -> [Int]
+eratostheneSieve n = eratostheneSieveAux [2..n]
+    where 
+        eratostheneSieveAux [] = []
+        eratostheneSieveAux (p:xs) = p : eratostheneSieveAux [x | x <- xs, x `rem` p /= 0]
 
 
-ordonataNat [ ] = True
-ordonataNat [ _ ] = True
-ordonataNat ( x : xs ) =
-    let 
-        y = head xs 
-    in x < y && ordonataNat xs
+and' l = foldr (&&) True l
 
+ordonataNat :: [Int] -> Bool
+ordonataNat [] = True
+ordonataNat [_] = True
+ordonataNat (x:y:xs) = x < y && ordonataNat xs
 
-ordonataNat1 [ ] = True
-ordonataNat1 [ _ ] = True
-ordonataNat1 l = and [x < y| (x,y) <- zip l (tail l)]
+ordonataNat' :: Ord a => [a] -> Bool
+ordonataNat' l = and [ x < y | (x,y) <- zip l (tail l) ]
 
-
-ordonata :: [a] -> (a -> a -> Bool) -> Bool
+ordonata :: [t] -> (t -> t -> Bool) -> Bool
 ordonata [] _ = True
-ordonata l p = and [p x y | (x,y) <- zip l (tail l)]
+ordonata l p = and' [ p x y | (x,y) <- zip l (tail l) ]
 
-(*<*) :: ( Integer , Integer ) -> ( Integer , Integer ) -> Bool
-(*<*) (a,b) (c,d) = a<c && b<d
-
-compuneList :: (b -> c) -> [(a -> b)] -> [(a -> c)]
-compuneList f lf =  map ( f . ) lf
-
-aplicaList a lf = map(\f -> f a) lf
+(*<*) :: (Integer, Integer) -> (Integer, Integer) -> Bool
+(*<*) (a,b) (c,d) = a < d && b < c 
 
 
-myzip3' a b c = map (\(x,(y, z)) -> (x, y, z))  (zip a (zip b c))
+compuneList :: (b -> c) -> [a -> b] -> [a -> c]
+compuneList f  =  map ( f . )
+
+aplicaList :: t -> [t -> b] -> [b]
+aplicaList arg l = map(\f ->  f arg) l
