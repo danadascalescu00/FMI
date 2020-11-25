4ef21da51562164ed9affc1ff9ca57549ffbb43f
Dăscălescu Dana <48893255+danadascalescu00@users.noreply.github.com>
2020-11-25 10:50:19 +0200

    Add files via upload

--- PD/Lab5.hs
+++ PD/Lab5.hs
@@ -1,50 +1,55 @@
-import Data.Char
-import Data.List
+import Data.Char ( isAlphaNum, toUpper )
+import Data.List (find)
+import Data.Maybe (fromMaybe)
 
 
--- 1.
 rotate :: Int -> [Char] -> [Char]
 rotate n str
-    | n < 0 = error "n is negative"
+    | n < 0 = error "n must be a positive integer"
     | n > length str = error "n is greater than the lenght of the given string"
-    | otherwise = (drop n str) ++ (take n str)
+    | otherwise = drop n str ++ take n str
 
--- 2.
+
 prop_rotate :: Int -> String -> Bool
 prop_rotate k str = rotate (l - m) (rotate m str) == str
                         where l = length str
                               m = if l == 0 then 0 else k `mod` l
 
--- 3.
+
+alphabet :: [Char]
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 makeKey :: Int -> [(Char, Char)]
 makeKey n = zip alphabet (rotate n alphabet)
 
--- 4.
+
 lookUp :: Char -> [(Char, Char)] -> Char
 lookUp ch [] = ch
-lookUp ch ((a,b):xs) = if ch == a then b else lookUp ch xs
--- lookUp ch str = fmap snd (find (\(x, _) -> x == ch) str)
+lookUp ch ((c1,c2):xs)
+    | ch == c1 = c2
+    | otherwise = lookUp ch xs
 
--- 5.
-encipher :: Int -> Char -> Char
-encipher n ch = lookUp ch (makeKey n)
+lookUp' :: Char -> [(Char, Char)] -> Char
+lookUp' ch [] = ch
+lookUp' ch ((a,b):xs) = if ch == a then b else lookUp ch xs
 
--- 6.
+lookUp2 :: Char -> [(Char, Char)] -> Char
+lookUp2 ch keys =  maybe ch snd (find (\ (x, _) -> x == ch) keys)
+
+
+enchiper :: Int -> Char -> Char
+enchiper n ch = lookUp ch $ makeKey n
+
 normalize :: String -> String
-normalize str = filter isAlphaNum  (map toUpper str)
+normalize str  = filter isAlphaNum $ map toUpper str
 
--- 7.
-encipherStr :: Int -> String -> String
-encipherStr n str = map (encipher n) (normalize str)
+enchiperStr :: Int -> String -> String
+enchiperStr n  str = map (enchiper n) (normalize str)
 
--- 8.
 reverseKey :: [(Char, Char)] -> [(Char, Char)]
 reverseKey = map(\(x,y) -> (y,x))
 
--- 9.
-decipher :: Int -> Char -> Char
-decipher n ch = lookUp ch (reverseKey (makeKey n)) 
+dechiper :: Int -> Char -> Char
+dechiper n ch = lookUp ch (reverseKey (makeKey n))
 
-decipherStr :: Int -> String -> String
-decipherStr n = map  (decipher n) . normalize
+dechiperStr :: Int -> String -> String 
+dechiperStr n  = map(dechiper n) 
