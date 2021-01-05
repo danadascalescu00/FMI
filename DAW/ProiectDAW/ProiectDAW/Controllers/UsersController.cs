76999d9039a564383464a83e18e976d6c7ece138
danadascalescu00 <dana.dascalescu@my.fmi.unibuc.ro>
2021-01-05 20:11:48 +0200

    add user controller

---
+++ DAW/ProiectDAW/ProiectDAW/Controllers/UsersController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProiectDAW.Models;
+using System.Data.Entity;
+
+namespace ProiectDAW.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class UsersController : Controller
+    {
+        private ApplicationDbContext ctx = new ApplicationDbContext();
+
+        // GET: Users
+        public ActionResult Index()
+        {
+            ViewBag.UsersList = ctx.Users.OrderBy(u => u.UserName).ToList();
+            return View();
+        }
+
+        public ActionResult Details(string id)
+        {
+            if (String.IsNullOrEmpty(id))
+                return HttpNotFound("Missing user id parameter!");
+
+            ApplicationUser user = ctx.Users.Include("Roles").FirstOrDefault(u => u.Id.Equals(id));
+            if(user != null)
+            {
+                ViewBag.UserRole = ctx.Roles.Find(user.Roles.First().RoleId).Name;
+                return View(user);
+            }
+            return HttpNotFound("Couldn't find the user with id " + id.ToString() + "!");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(string id)
+        {
+            if (String.IsNullOrEmpty(id))
+                return HttpNotFound("Missing user id parameter!");
+
+            UserViewModel uvm = new UserViewModel();
+            uvm.User = ctx.Users.Find(id);
+            uvm.RoleName = ctx.Roles.Find(uvm.User.Roles.First().RoleId).Name;
+            return View(uvm);
+        }
+
+        [HttpPut]
+        public ActionResult Edit(string id, UserViewModel uvm)
+        {
+            try
+            {
+                ApplicationUser user = ctx.Users.Find(id);
+
+                if(TryUpdateModel(user))
+                {
+                    var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(ctx));
+                    foreach(var r in ctx.Roles.ToList())
+                    {
+                        userManager.RemoveFromRole(user.Id, r.Name);
+                    }
+                    userManager.AddToRole(user.Id, uvm.RoleName);
+                    ctx.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }catch(Exception e)
+            {
+                return View(uvm);
+            }
+        }
+    }
+}
---
+++ DAW/ProiectDAW/ProiectDAW/Models/UserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProiectDAW.Models
+{
+    public class UserViewModel
+    {
+        public ApplicationUser User { get; set; }
+        public string RoleName { get; set; }
+
+    }
+}
