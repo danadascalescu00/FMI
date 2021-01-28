0b084d1cc53e084828a94d2b6eba58938cb7ccc5
Dăscălescu Dana <48893255+danadascalescu00@users.noreply.github.com>
2021-01-28 12:06:39 +0200

    Add files via upload

--- SGBD/Laborator6.sql
+++ SGBD/Laborator6.sql
@@ -516,19 +516,62 @@ ADD PRIMARY KEY (employee_id);
 --       - codul managerului se va obtine cu ajutorul unei functii stocate in pachet care va avea ca parametrii numele si prenumele managerului); 
 --       - codul  departamentului  va  fi  obtinut  cu  ajutorul  unei  functii  stocate  în  pachet,  dandu-se ca parametru numele acestuia; 
 --       - codul jobului va fi obtinut cu ajutorul unei functii stocate in pachet, dandu-se ca parametru numele acesteia
+--    b) o procedura care determina mutarea in alt departament a unui angajat (se dau ca parametrii numele si prenumele angajatului,  
+--    respectiv numele departamentului, numele jobului si numele si prenumele managerului acestuia): 
+--       - se vor actualiza informatiile angajatului: 
+--       - codul de departament(se va obtine cu ajutorul functiei corespunzatoare definita la punctul a); 
+--       - codul jobului (se va obtine cu ajutorul functiei corespunzatoare definita la punctul a); 
+--       - codul managerului (se va obtine cu ajutorul functiei corespunzatoare definita la punctul a); 
+--       - salariul va fi cel mai mic salariu din noul departament, pentru noul job daca acesta este mai mare decat salariul curent; altfel
+--     se va pastra salariul curent; 
+--       - comisionul va fi cel mai mic comision din acel departament, pentru acel job; 
+--       - data angajarii va fi data curenta; 
+--       - se vor inregistra informatii corespunzatoare in istoricul joburilor. 
+--    c) o functie care intoarce numarul de subalterni directi sau indirecti ai unui angajat al carui nume si prenume sunt date ca parametrii; 
+--    d) o procedura prin care se actualizeaza cu o valoare data ca parametru salariul unui angajat al carui nume este dat ca parametru: 
+--      - se va verifica daca valoarea data pentru salariu respecta limitele impuse pentru acel job; 
+--      - daca sunt mai multi angajati care au acelasi nume, atunci se va afisa un mesaj corespunzator si de asemenea se va afisa lista acestora; 
+--      - daca nu exista angajati cu numele dat, atunci se va afisa un mesaj corespunzator; 
+--    e) un  cursor  care  obtine  lista  angajatilor  care  lucreaza  pe  un  job  al  carui  cod  este  dat  ca parametru; 
+--    f) un cursor care obtine lista tuturor joburilor din companie;
+--    g) o procedura care utilizeaza cele doua cursoare definite anterior si obtine pentru fiecare job numele acestuia si lista angajatilor 
+--    care lucreaza in prezent pe acel job. In plus, pentru fiecare angajat sa se specifice daca in trecut a mai avut sau nu jobul respectiv. 
+
+CREATE TABLE job_history_dda AS
+    SELECT * FROM job_history;
 
 CREATE SEQUENCE sec_dda;
 
-DESC JOBS;
-SELECT * FROM JOBS;
+DESC emp_dda;
+DESC jobs;
+DESC job_history;
 
 CREATE OR REPLACE PACKAGE pachet1_dda AS
-    FUNCTION f_cod_job(job_name jobs.job_title%TYPE)
-    RETURN jobs.job_id%TYPE;
-    FUNCTION f_salariu_minim(dept_id emp_dda.department_id%TYPE)
-    RETURN emp_dda.salary%TYPE;
-    FUNCTION f_cod_manager(prenume emp_dda.first_name%TYPE, nume emp_dda.last_name%TYPE)
-    RETURN emp_dda.employee_id%TYPE;
+    FUNCTION f_cod_job(job_name jobs.job_title%TYPE) RETURN jobs.job_id%TYPE;
+    FUNCTION f_salariu_minim(dept_id emp_dda.department_id%TYPE, id_job emp_dda.job_id%TYPE) RETURN emp_dda.salary%TYPE;
+    FUNCTION f_commission_minim(dept_id emp_dda.department_id%TYPE, id_job emp_dda.job_id%TYPE) RETURN emp_dda.commission_pct%TYPE;
+    FUNCTION f_cod_manager(prenume emp_dda.first_name%TYPE, nume emp_dda.last_name%TYPE) RETURN emp_dda.employee_id%TYPE;
+    FUNCTION f_cod_departament(nume_departament dep_dda.department_name%TYPE) RETURN emp_dda.department_id%TYPE;
+    PROCEDURE adauga_angajat(nume emp_dda.last_name%TYPE, prenume emp_dda.first_name%TYPE, adresa_email emp_dda.email%TYPE, 
+        nr_telefon emp_dda.phone_number%TYPE, nume_job jobs.job_title%TYPE, nume_departament departments.department_name%TYPE,
+        prenume_manager emp_dda.first_name%TYPE,nume_manager emp_dda.last_name%TYPE);
+    PROCEDURE mutare_angajat(nume emp_dda.last_name%TYPE, prenume emp_dda.first_name%TYPE, nume_job jobs.job_title%TYPE, 
+        nume_departament departments.department_name%TYPE,prenume_manager emp_dda.first_name%TYPE,nume_manager emp_dda.last_name%TYPE);
+    FUNCTION f_cod_angajat(prenume emp_dda.first_name%TYPE, nume emp_dda.last_name%TYPE) RETURN emp_dda.employee_id%TYPE;
+    FUNCTION f_numar_subalterni(prenume emp_dda.first_name%TYPE, nume emp_dda.last_name%TYPE) RETURN NUMBER;
+    PROCEDURE actualizare_salariu(nume emp_dda.last_name%TYPE, valoare emp_dda.salary%TYPE);
+        
+    CURSOR c_angajati_joburi(parametru jobs.job_id%TYPE)
+    RETURN emp_dda%ROWTYPE
+    IS
+    SELECT * FROM emp_dda WHERE job_id = parametru;
+    
+    CURSOR c_joburi 
+    IS
+    SELECT * FROM jobs;
+    
+    PROCEDURE lista_angajati_per_job;
+    
 END pachet1_dda;
 /
 
@@ -549,21 +592,45 @@ CREATE OR REPLACE PACKAGE BODY pachet1_dda AS
             RETURN -3;
     END f_cod_job;
     
-    FUNCTION f_salariu_minim(dept_id emp_dda.department_id%TYPE)
-        RETURN emp_dda.salary%TYPE IS salariu emp_dda.salary%TYPE;
+    FUNCTION f_salariu_minim(dept_id emp_dda.department_id%TYPE, id_job emp_dda.job_id%TYPE)
+        RETURN emp_dda.salary%TYPE 
+    IS 
+        salariu emp_dda.salary%TYPE;
+        exceptie EXCEPTION;
     BEGIN
         SELECT MIN(salary) INTO salariu
         FROM emp_dda
-        WHERE department_id = dept_id;
-    RETURN salariu;
+        WHERE department_id = dept_id
+            AND job_id = id_job;
+        IF salariu = NULL THEN
+            RAISE exceptie;
+        END IF;
+        RETURN salariu;
     EXCEPTION
-        WHEN NO_DATA_FOUND THEN
-            DBMS_OUTPUT.PUT_LINE('Department id does not exist!');
+        WHEN exceptie THEN
+            DBMS_OUTPUT.PUT_LINE('department_id and job_id combination does not exist!');
             RETURN -1;
         WHEN OTHERS THEN
             RAISE_APPLICATION_ERROR(-20001,'An error was encountered - '||SQLCODE||' -ERROR- '||SQLERRM);
             RETURN -3;
     END f_salariu_minim;
+    
+    FUNCTION f_commission_minim(dept_id emp_dda.department_id%TYPE, id_job emp_dda.job_id%TYPE) 
+        RETURN emp_dda.commission_pct%TYPE IS commission emp_dda.commission_pct%TYPE;
+    BEGIN
+        SELECT MIN(NVL(commission_pct,0)) INTO commission
+        FROM emp_dda
+        WHERE department_id = dept_id
+            AND job_id = id_job;
+        RETURN commission;
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            DBMS_OUTPUT.PUT_LINE('department_id and job_id combination does not exist!');
+            RETURN -1;
+        WHEN OTHERS THEN
+            RAISE_APPLICATION_ERROR(-20001,'An error was encountered - '||SQLCODE||' -ERROR- '||SQLERRM);
+            RETURN -3;
+    END f_commission_minim;
     
     FUNCTION f_cod_manager(prenume emp_dda.first_name%TYPE, nume emp_dda.last_name%TYPE) 
     RETURN emp_dda.employee_id%TYPE IS manager_id emp_dda.employee_id%TYPE;
@@ -581,9 +648,182 @@ CREATE OR REPLACE PACKAGE BODY pachet1_dda AS
             RAISE_APPLICATION_ERROR(-20001,'An error was encountered - '||SQLCODE||' -ERROR- '||SQLERRM);
             RETURN -3;  
     END f_cod_manager;
+    
+    FUNCTION f_cod_departament(nume_departament dep_dda.department_name%TYPE)
+    RETURN emp_dda.department_id%TYPE IS cod_departament emp_dda.department_id%TYPE;
+    BEGIN
+        SELECT department_id INTO cod_departament
+        FROM dep_dda
+        WHERE department_name = nume_departament;
+        RETURN cod_departament;
+    END f_cod_departament;
+    
+    FUNCTION f_cod_angajat(prenume emp_dda.first_name%TYPE, nume emp_dda.last_name%TYPE) 
+    RETURN emp_dda.employee_id%TYPE IS cod_angajat emp_dda.employee_id%TYPE;
+    BEGIN
+        SELECT employee_id INTO cod_angajat
+        FROM emp_dda
+        WHERE first_name = prenume AND last_name = nume;
+        RETURN cod_angajat;
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            DBMS_OUTPUT.PUT_LINE('Nu exista niciun angajat cu numele si prenumele dat!');
+            RETURN -1;
+        WHEN OTHERS THEN
+            DBMS_OUTPUT.PUT_LINE(SQLCODE || ' - ' || SQLERRM);
+            RETURN -3;
+    END f_cod_angajat;
+    
+    FUNCTION f_numar_subalterni(prenume emp_dda.first_name%TYPE, nume emp_dda.last_name%TYPE)
+    RETURN NUMBER IS v_numar_subalterni NUMBER;
+    BEGIN
+        SELECT COUNT(*) INTO v_numar_subalterni
+        FROM emp_dda
+        START WITH employee_id = f_cod_angajat(prenume, nume)
+        CONNECT BY PRIOR employee_id = manager_id;
+        RETURN v_numar_subalterni;
+    END f_numar_subalterni;
+    
+    PROCEDURE adauga_angajat(nume emp_dda.last_name%TYPE, prenume emp_dda.first_name%TYPE, adresa_email emp_dda.email%TYPE, 
+        nr_telefon emp_dda.phone_number%TYPE, nume_job jobs.job_title%TYPE, nume_departament departments.department_name%TYPE,
+        prenume_manager emp_dda.first_name%TYPE,nume_manager emp_dda.last_name%TYPE)
+    IS
+        cod_job emp_dda.job_id%TYPE;
+        salariu_minim emp_dda.salary%TYPE;
+        cod_departament emp_dda.department_id%TYPE;
+        cod_manager emp_dda.employee_id%TYPE;
+    BEGIN
+        cod_job := f_cod_job(nume_job);
+        cod_departament := f_cod_departament(nume_departament);
+        salariu_minim := f_salariu_minim(cod_departament, cod_job);
+        cod_manager := f_cod_manager(prenume_manager, nume_manager);
+        
+        INSERT INTO emp_dda
+        VALUES(sec_dda.NEXTVAL, prenume, nume, adresa_email, nr_telefon, 
+            SYSDATE, cod_job , salariu_minim, 0, cod_manager, cod_departament);
+    END adauga_angajat;
+    
+    PROCEDURE mutare_angajat(nume emp_dda.last_name%TYPE, prenume emp_dda.first_name%TYPE, nume_job jobs.job_title%TYPE, 
+        nume_departament departments.department_name%TYPE,prenume_manager emp_dda.first_name%TYPE,nume_manager emp_dda.last_name%TYPE)
+    IS
+        emp_id               emp_dda.employee_id%TYPE;
+        cod_job              emp_dda.job_id%TYPE;
+        salariu_curent       emp_dda.salary%TYPE;
+        salariu_minim        emp_dda.salary%TYPE;
+        commission_pct_minim emp_dda.commission_pct%TYPE;
+        cod_departament      emp_dda.department_id%TYPE;
+        cod_manager          emp_dda.employee_id%TYPE;
+    BEGIN
+        emp_id := f_cod_angajat(prenume, nume);
+        cod_job := f_cod_job(nume_job);
+        cod_departament := f_cod_departament(nume_departament);
+        salariu_minim := f_salariu_minim(cod_departament, cod_job);
+        commission_pct_minim := f_commission_minim(cod_departament, cod_job);
+        cod_manager := f_cod_manager(prenume_manager, nume_manager);
+        
+        SELECT salary INTO salariu_curent
+        FROM emp_dda
+        WHERE employee_id = emp_id;
+        
+        INSERT INTO job_history_dda
+        VALUES (emp_id, SYSDATE, SYSDATE + 365, cod_job, cod_departament);
+        
+        IF salariu_minim > salariu_curent THEN
+            salariu_curent := salariu_minim;
+        END IF;
+        
+        UPDATE emp_dda
+        SET 
+            job_id = cod_job,
+            department_id = cod_departament,
+            salary = salariu_curent,
+            commission_pct = commission_pct_minim,
+            manager_id = cod_manager,
+            hire_date = SYSDATE
+        WHERE employee_id = emp_id;
+    END mutare_angajat;
+    
+    PROCEDURE actualizare_salariu(nume emp_dda.last_name%TYPE, valoare emp_dda.salary%TYPE)
+    IS
+        emp_id emp_dda.employee_id%TYPE;
+        id_job emp_dda.job_id%TYPE;
+        salariu emp_dda.salary%TYPE;
+        TYPE nested_table_type IS TABLE OF emp_dda.employee_id%TYPE;
+        t_ids nested_table_type;
+        salariu_minim jobs.min_salary%TYPE;
+        salariu_maxim jobs.max_salary%TYPE;
+        exceptie EXCEPTION;
+    BEGIN
+        SELECT employee_id, job_id, salary
+        INTO emp_id, id_job, salariu
+        FROM employees
+        WHERE last_name = nume;
+        
+        SELECT min_salary, max_salary
+        INTO salariu_minim, salariu_maxim
+        FROM jobs
+        WHERE job_id = id_job;
+        
+        IF salariu + valoare > salariu_minim AND salariu + valoare < salariu_maxim THEN
+            UPDATE emp_dda
+            SET salary = salary + valoare
+            WHERE employee_id = emp_id;
+        ELSE RAISE exceptie;
+        END IF;
+        
+    EXCEPTION
+        WHEN TOO_MANY_ROWS THEN
+            DBMS_OUTPUT.PUT_LINE('Exista mai multi angajati cu numele ' || nume);
+            SELECT employee_id
+            BULK COLLECT INTO t_ids
+            FROM emp_dda
+            WHERE last_name = nume;
+            FOR i IN t_ids.FIRST..t_ids.LAST LOOP
+                DBMS_OUTPUT.PUT(' ' || t_ids(i) || ' ');
+            END LOOP;
+            DBMS_OUTPUT.NEW_LINE;
+        WHEN exceptie THEN
+            DBMS_OUTPUT.PUT_LINE('Valoarea data nu respecta limitele impuse pentru acest job!');
+    END;
+    
+    PROCEDURE lista_angajati_per_job 
+    IS
+        job_precedent NUMBER;
+    BEGIN
+        FOR job IN c_joburi LOOP
+            FOR angajat IN c_angajati_joburi(job.job_id) LOOP
+                DBMS_OUTPUT.PUT_LINE('Angajat ' || angajat.employee_id || ' : ' || angajat.first_name || ' ' || angajat.last_name);
+                
+                SELECT COUNT(*) INTO job_precedent
+                FROM job_history
+                WHERE employee_id = angajat.employee_id
+                    AND job_id = job.job_id
+                    AND end_date < SYSDATE;
+                    
+                IF job_precedent <> 0  THEN
+                    DBMS_OUTPUT.PUT_LINE('A mai avut job-ul precedent.');
+                END IF;
+                DBMS_OUTPUT.NEW_LINE;
+            END LOOP;
+        END LOOP;
+    EXCEPTION
+        WHEN INVALID_CURSOR THEN
+            DBMS_OUTPUT.PUT_LINE(SQLCODE || ' - ' || SQLERRM);
+    END lista_angajati_per_job;
 END pachet1_dda;
 /
 
+SELECT pachet1_dda.f_cod_angajat('Steven', 'King') FROM DUAL; -- o functie care primeste ca parametru numele si prenumele unui angajat si intoarce codul angajatului
+SELECT pachet1_dda.f_numar_subalterni('Steven', 'King') FROM DUAL;
 SELECT pachet1_dda.f_cod_job('Accountant') FROM DUAL; -- o functie care primeste ca parametru un job_name si returneaza un job_id
-SELECT pachet1_dda.f_salariu_minim(50) FROM DUAL; -- o functie ce primeste ca parametru un job_id si returneaza salariul minim pe respectivul departament
+-- o functie ce primeste ca parametru un job_id si un department_id si returneaza salariul minim pe respectivul departament si job
+SELECT pachet1_dda.f_salariu_minim(40, 'HR_REP') FROM DUAL; 
+-- o functie ce primeste ca parametru un job_id si un department_id si returneaza commission_pct minim pe respectivul departament si job
+SELECT pachet1_dda.f_commission_minim(40, 'HR_REP') FROM DUAL; 
 SELECT pachet1_dda.f_cod_manager('Steven', 'King') FROM DUAL; -- o functie ce primeste ca parametru numele si prenumele managerului si intoarce codul acestuia
+
+SET SERVEROUTPUT ON
+BEGIN
+    pachet1_dda.lista_angajati_per_job;
+END;
+/
